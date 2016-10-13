e5002e9cb7b925e96730fb5ce3a92b6dd54e7fa6
A. Pic <dev@apic.name>
2016-10-13 16:44:25 +0200

    Weitere Deklaration.

--- botforth.h
+++ botforth.h
@@ -47,5 +47,6 @@ struct vector *load_file(char *name);
 
 extern struct vector *word_load(char *name, char *ns);
 extern int infoblock_set_accesslevel(struct vector *word, int al);
+extern int infoblock_set_name(struct vector * word, char *name);
 
 #endif
