af0ec22d8171466a46a962b59f893596e02c441f
A. Pic <dev@apic.name>
2016-10-18 20:05:23 +0200

    Meldung bei Dateianhang.

--- intcommands.c
+++ intcommands.c
@@ -1124,6 +1124,8 @@ void bf_c_file_append()
 	char *filename = vector_pop_string(dstack);
 	char *text = vector_pop_string(dstack);
 	FILE *file;
+        fprintf(stderr, "Dateianhangsversuch mit Level %d: „%s“\n",
+               accesslevel, filename);
 	if (accesslevel < 3) {
 		if (filename && text) {
 			file = fopen(filename, "a");
