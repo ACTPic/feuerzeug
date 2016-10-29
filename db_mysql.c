6cb872334a498d3e9d56b93fd35bd87bbd358977
A. Pic <dev@apic.name>
2016-10-29 23:19:24 +0200

    Aufräumaktion.

--- db_mysql.c
+++ db_mysql.c
@@ -3,79 +3,47 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <string.h>
-//#include <linux/list.h>
 #include "botforth.h"
 
 // MySQL-Krempel
-extern MYSQL_FIELD *fld;
 extern MYSQL mysql_read;
 extern MYSQL mysql_write;
-extern MYSQL_RES *res;
-extern MYSQL_ROW row;
 
 struct vector *sql_load(char *name)
 {
-	char *query;
-	MYSQL_RES *res;
-	char *buffer=0, *nameescape;
-	struct node *n;
-	struct vector *v;
-	unsigned i;
-	nameescape = malloc(strlen(name) * 3 * sizeof(char));
-	mysql_escape_string(nameescape, name, strlen(name));
-	query = malloc((39 + strlen(nameescape) + 1) * sizeof(char));
-	sprintf(query, "select * from calc where eintrag='%s'",
-		nameescape);
-	if (mysql_real_query(&mysql_read, query, strlen(query))) {
-		printf
-		    ("sql query fehlgeschlagen bei laden von %s mittels %s\n",
-		     name, query);
-		free(query);
-		free(nameescape);
-		v = vector_create();
-		n = node_create(buffer, BF_TYPE_STRING);
-		vector_put(v, fld->name, n);
-		return((struct vector *)n);
-	} else {
-		free(query);
-		free(nameescape);
-		if (!(res = mysql_store_result(&mysql_read))) {
-			printf("sql store results fehlgeschlagen\n");
-			exit(0);
-		}
-		if (res) {
-			if((row = mysql_fetch_row(res))) {
-				v = vector_create();
-				for (i = 0; i < mysql_num_fields(res); i++) {
-					if (row[i]) {
-						fld =
-						    mysql_fetch_field_direct
-						    (res, i);
-						buffer =
-						    malloc(strlen(row[i]) +
-							   1);
-						sprintf(buffer, "%s",
-							row[i]);
-						n = node_create(buffer,
-								BF_TYPE_STRING);
-						vector_put(v, fld->name,
-							   n);
-					}
-				}
-				// todo: was ist mit dem freeen von row und fld usw.?
-				mysql_free_result(res);
-				//free(row);
-				//free(fld);
-				printf("vector in file:");
-				return v;
-			} else {
-				// todo: ist das hier ok so?
-				mysql_free_result(res);
-			}
-		} else {
-			printf("kein mysql-res auf stack gefunden.\n");
-		}
-	}
-	return NULL;
-}
+	 MYSQL_RES *res;
+	 struct vector *v;
 
+	 char *nameescape = malloc(strlen(name) * 3 * sizeof(char));
+	 mysql_escape_string(nameescape, name, strlen(name));
+	 char *query =
+	     malloc((39 + strlen(nameescape) + 1) * sizeof(char));
+	 sprintf(query, "select * from calc where eintrag='%s'",
+		 nameescape);
+
+	 assert(!mysql_real_query(&mysql_read, query, strlen(query)));
+	 free(query);
+	 free(nameescape);
+	 assert((res = mysql_store_result(&mysql_read)));
+
+	 MYSQL_ROW row = mysql_fetch_row(res);
+	 if (!row) {
+		  mysql_free_result(res);
+		  return 0;
+	 }
+
+	 v = vector_create();
+	 for (unsigned i = 0; i < mysql_num_fields(res); i++) {
+		  if (row[i]) {
+			   MYSQL_FIELD *fld;
+			   fld = mysql_fetch_field_direct(res, i);
+			   char *buf = malloc(strlen(row[i]) + 1);
+			   strcpy(buf, row[i]);
+			   struct node *n;
+			   n = node_create(buf, BF_TYPE_STRING);
+			   vector_put(v, fld->name, n);
+		  }
+	 }
+	 mysql_free_result(res);
+	 return v;
+}
