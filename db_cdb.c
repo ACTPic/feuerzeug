8aaebe9253d3acbe4d430b09adcca2de3c100171
A. Pic <dev@apic.name>
2016-11-02 16:55:23 +0100

    Datenbankfeld „type“ hinfrickeln.

--- db_cdb.c
+++ db_cdb.c
@@ -40,6 +40,8 @@ void build_node(char *name, struct vector *v, char *sub)
 		struct node *node;
 		if (!strcmp(sub, "auth"))
 			node = node_create("0", BF_TYPE_STRING);
+		else if (!strcmp(sub, "type") && !*val)
+			node = node_create("0", BF_TYPE_STRING);
 		else
 			node = node_create(val, BF_TYPE_STRING);
 		vector_put(v, sub, node);
