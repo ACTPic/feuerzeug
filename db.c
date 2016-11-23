6f55a2505c594bef6066c4280ff8be83c3d7b8a8
A. Pic <dev@apic.name>
2016-11-23 17:37:04 +0100

    „getopt“ nutzen; Option für nur rohen Inhalt hinzufügen.

--- db.c
+++ db.c
@@ -1,23 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
 #include "vector.h"
 #include "botforth.h"
 
 extern void cdb_startup();
 extern void bdb_init();
 
+void puthelp()
+{
+	printf("Argumente:\n"
+	       "        --help                    Diese Hilfe\n"
+	       "        --dump    [Eintrag]       Datenbankeintrag mit Spaltennamen anzeigen\n"
+	       "        --content [Eintrag]       Das Feld „Inhalt“ ausgeben\n");
+}
+
 int main(int argc, char **argv)
 {
-	if (argc != 2) {
-		fprintf(stderr, "%s [Eintrag]\n", argv[0]);
-		exit(1);
+	if (argc < 2) {
+		puthelp();
+		return 1;
 	}
 
 	cdb_startup();
 	bdb_init();
 
-	struct vector *v = load_file(argv[1]);
-	debug(v);
+	for (;;) {
+		int option_index = 0;
+		static struct option long_options[] = {
+			{"help", no_argument, 0, 'h'},
+			{"dump", required_argument, 0, 'd'},
+			{"content", required_argument, 0, 'c'},
+			{0, 0, 0, 0}
+		};
+
+		int c = getopt_long(argc, argv, "d:c:h",
+				    long_options, &option_index);
+		if (c == -1)
+			break;
+
+		switch (c) {
+		case 'h':
+			puthelp();
+			return 1;
+		case 'd':
+			{
+				struct vector *v = load_file(optarg);
+				debug(v);
+				break;
+			}
+		case 'c':
+			{
+				struct vector *v = load_file(optarg);
+				char *inhalt =
+				    vector_pick_string(v, "inhalt");
+				if (!inhalt)
+					break;
+				printf("%s", inhalt);
+				break;
+			}
+		case '?':
+			break;
+		}
+	}
 
 	return 0;
 }
