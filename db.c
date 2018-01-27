1421fb4efe44a21516366d9a30c27d16227891b6
A. Pic <dev@apic.name>
2018-01-27 19:31:56 +0100

    CDB-Indexer mergen.

--- db.c
+++ db.c
@@ -1,11 +1,23 @@
+// Kredite: Michael Tokarev (tinycdb),  Daniel Julius Bernstein (CDB)
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <stdarg.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <getopt.h>
+#include <unistd.h>
+#include <sys/stat.h>
 #include <inttypes.h>
+#include <sys/types.h>
+#include "cdb.h"
 #include "vector.h"
 #include "botforth.h"
+
+
+#define blen 2048
+static unsigned char buf[blen];
 
 extern void cdb_startup();
 extern struct vector *cdb_load_random();
@@ -22,8 +34,63 @@ void puthelp()
 	       "    --content [Eintrag]           Das Feld „Inhalt“ ausgeben\n"
 	       "    --allcont                     Das Feld „Inhalt“ aller Einträge ausgeben\n"
 	       "    --anlcont                     ^ mit „\\n“ statt „\\0“ als Trenner\n"
-	       "    --write   [Eintrag] [Inhalt]  Datenbankeintrag schreiben\n");
+	       "    --genindex_cdb                Indexdatei für CDB-Datenbank anlegen\n");
 }
+
+static void
+fget(FILE * f, unsigned char *b, unsigned len, unsigned *posp,
+     unsigned limit)
+{
+	if (posp && limit - *posp < len)
+		exit(1);
+	if (fread(b, 1, len, f) != len) {
+		if (ferror(f))
+			exit(1);
+		fprintf(stderr, "unable to read: short file\n");
+		exit(2);
+	}
+	if (posp)
+		*posp += len;
+}
+
+static int fskip(FILE * fi, unsigned len, unsigned *posp, unsigned limit)
+{
+	while (len > blen) {
+		fget(fi, buf, blen, posp, limit);
+		len -= blen;
+	}
+	if (len) {
+		fget(fi, buf, len, posp, limit);
+	}
+	return 0;
+}
+
+static void genindex_cdb()
+{
+	unsigned eod, klen, vlen;
+	unsigned pos = 0;
+	FILE *f, *fo;
+	if (!(f = fopen("calc.cdb", "r")))
+		exit(1);
+	else if (!(fo = fopen("calc.cdb.index", "w")))
+		exit(1);
+	fget(f, buf, blen, &pos, blen);
+	eod = cdb_unpack(buf);
+	while (pos < eod) {
+		cdb_pack(pos, buf);
+		fwrite(buf, 4, 1, fo);
+		fget(f, buf, 8, &pos, eod);
+		klen = cdb_unpack(buf);
+		vlen = cdb_unpack(buf + 4);
+		if (fskip(f, klen, &pos, eod) != 0)
+			exit(1);
+		if (fskip(f, vlen, &pos, eod) != 0)
+			exit(1);
+	}
+	if (fflush(fo) < 0 || pos != eod)
+		exit(1);
+}
+
 
 static void build_node(char *name, struct vector *v, char *content)
 {
@@ -98,10 +165,11 @@ int main(int argc, char **argv)
 			{"allcont", no_argument, 0, 'C'},
 			{"anlcont", no_argument, 0, 'N'},
 			{"write", required_argument, 0, 'w'},
+			{"genindex_cdb", no_argument, 0, 'g'},
 			{0, 0, 0, 0}
 		};
 
-		int c = getopt_long(argc, argv, "d:c:hDrCNw:",
+		int c = getopt_long(argc, argv, "d:c:hdrCNw:g",
 				    long_options, &option_index);
 		if (c == -1)
 			break;
@@ -160,6 +228,9 @@ int main(int argc, char **argv)
 			}
 		case 'w':
 			eintrag = optarg;
+			break;
+		case 'g':
+			genindex_cdb();
 			break;
 		case '?':
 			break;
